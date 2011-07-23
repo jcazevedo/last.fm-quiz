249d2027415c8c019e167e8719488bf6473b21d1
Joao Azevedo <joao.c.azevedo@gmail.com>
2011-07-23 23:06:44 +0100

    Switching to rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LastFmQuiz::Application.config.secret_token = 'ec90a7af2189f12b49deb3113ae9e6a6e5ddff260f840be78a9d4b873fea69e7aab88ced2de8dcdd7d6a3fb9065b38c94346a143b77941adb7879bd2de5b479b'
